9a1daf6fbbf1d2d696f9c4781b2b7656f510de22
tangdy <tdy853839625@qq.com>
2024-08-10 14:21:22 +0800

    fix: 打通流程

--- websocket_server.py
+++ websocket_server.py
@@ -4,17 +4,19 @@ FilePath: /UE_Python_Sdk_Server/websocket_server.py
 '''
 import asyncio
 import websockets
-from dll_interface import init_msdk, speak_by_audio
-# from dll_interface_mock import init_msdk, speak_by_audio
-# from dll_doc_cdoe import init_msdk
+import aioredis
 from message_handle import messageHandler
 import json
-import asyncio
-from functools import partial
+import os
 import uuid
 import wave
-import os
 
+# 读取 JSON 文件
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+redis_host = config['REDIS_HOST']
+redis_port = config['REDIS_PORT']
+redis_password = config['REDIS_PASSWORD']
 def initialize_audio_file(client_id):
     # 确保目录存在
     if not os.path.exists('audio_logs'):
@@ -37,11 +39,8 @@ def finalize_audio_file(wf):
 # 用于存储连接的全局字典
 connected = {}
 
-async def msdk_handler(websocket, path):
-    # 生成一个随机ID 
-    # 生成一个随机的UUID
+async def msdk_handler(websocket):
     random_id = uuid.uuid4()
-    # 将UUID转换为字符串格式
     random_id_str = str(random_id)
     connected[random_id_str] = {
         'websocket': websocket,
@@ -58,25 +57,70 @@ async def msdk_handler(websocket, path):
     try:
         async for message in websocket:
             data = json.loads(message)
-            # 设置默认值防止报错
-            # print(data)
             await messageHandler(data, connected[random_id_str])
     except websockets.exceptions.ConnectionClosed:
         print(f"连接关闭: {random_id_str}")
     finally:
-        # 执行清理工作
-        # 关闭UE进程
         finalize_audio_file(connected[random_id_str]['wav_file'])
         if random_id_str in connected:
             del connected[random_id_str]
         print(f"Cleaned up resources for Client ID: {random_id_str}")
 
+async def redis_subscriber_start_websocket(redis):
+    channel_name = 'videohuman'  # 你需要订阅的频道名称
+    pubsub = redis.pubsub()
+    await pubsub.subscribe(channel_name)
+    try:
+        async for message in pubsub.listen():
+            print(f"来自Redis的消息 {message}")
+            if message['type'] == 'message':
+                # 解析消息内容 这里是初始化一个UE
+                data = json.loads(message['data'])
+                print(f"收到消息: {data}")
+                # 检查消息类型
+                if data.get('type') == 'init':
+                    # 获取 URI
+                    print(f"连接到 {data.get('url')}")
+                    uri = data.get('url')  # 假设有一个默认值
+                    # 启动 WebSocket 客户端
+                    await start_websocket_client(uri)
+    finally:
+        await pubsub.unsubscribe(channel_name)
+
+async def start_websocket_client(uri):
+    print(f"连接到 {uri}")
+    async with websockets.connect(uri) as websocket:
+        random_id = uuid.uuid4()
+        random_id_str = str(random_id)
+        connected[random_id_str] = {
+            'websocket': websocket,
+            'client_id': "",
+            "audio_buffer": bytearray(),
+            "frame_id": 0,
+            "is_final": False,
+            "audioDone": True,
+            "id": random_id_str
+        }
+        wf = initialize_audio_file(connected[random_id_str]['client_id'])
+        connected[random_id_str]['wav_file'] = wf
+
+        try:
+            async for message in websocket:
+                data = json.loads(message)
+                print(f"收到websocket消息: {data['type']}")
+                await messageHandler(data, connected[random_id_str])
+        except websockets.exceptions.ConnectionClosed as e:
+            print(f"WebSocket connection closed with error: {e}")
+
+
 
 async def main():
-    async with websockets.serve(msdk_handler, "0.0.0.0", 8765):
-        print("WebSocket服务器启动在 ws://localhost:8765")
-        await asyncio.Future()  # 运行直到被取消
+    redis = aioredis.from_url(f'redis://:{redis_password}@{redis_host}:{redis_port}', encoding='utf-8')
+    redis_task = asyncio.create_task(redis_subscriber_start_websocket(redis))
+    server_task = websockets.serve(msdk_handler, "0.0.0.0", 8765)
+    
+    print("WebSocket服务器启动在 ws://localhost:8765")
+    await asyncio.gather(server_task, redis_task)
 
 if __name__ == "__main__":
     asyncio.run(main())
-
